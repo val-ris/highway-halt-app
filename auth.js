4c1310036d8ed20085707dded18e9b7dbdf1f138
val-ris <valeristhunder@gmail.com>
2025-06-11 16:35:44 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,7 @@
 // Firebase Auth services
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase';
+
+export const login = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
