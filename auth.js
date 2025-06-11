fb5798c59dc0b54fa454ee36038e68a01c40eb81
val-ris <valeristhunder@gmail.com>
2025-06-11 16:53:51 +0530

    Delete auth.js

--- auth.js
+++
@@ -1,7 +0,0 @@
-// Firebase Auth services
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from './firebase';
-
-export const login = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
