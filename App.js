b3dda346a484e2a3e7afc61d90a8a20d17bef7a0
val-ris <valeristhunder@gmail.com>
2025-06-11 16:32:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
 // Entry point for Highway Halt App
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
